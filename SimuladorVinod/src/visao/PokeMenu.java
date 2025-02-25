2e732f57d488ce32e04e2ddeb4413c360974926b
cauab <cauabassin@id.uff.br>
2025-02-25 17:02:09 -0300

    Finalizado o PokeMenu

--- SimuladorVinod/src/visao/PokeMenu.java
+++ SimuladorVinod/src/visao/PokeMenu.java
@@ -5,8 +5,11 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.TitledBorder;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
 
 import main.UnidadeControle;
+import modelo.MemoriaPrincipal;
 import modelo.registradores.Registrador;
 import util.Conversoes;
 
@@ -39,6 +42,11 @@ public class PokeMenu {
 	private JButton btnAplicarReg;
 	private UnidadeControle uc;
 	private Registrador selectedReg;
+	private JTextField txtBinarioMem;
+	private JPanel panelRegistradores_1;
+	private JLabel lblEndereo;
+	private JSpinner spnDecimalMem;
+	private JTextField spnEndereco;
 
 	public PokeMenu(UnidadeControle uc) {
 		this.uc = uc;
@@ -61,7 +69,7 @@ public class PokeMenu {
 				uc.setPause(false);
 			}
 		});
-		frame.setBounds(100, 100, 640, 480);
+		frame.setBounds(100, 100, 337, 374);
 		frame.setTitle("Alterar valores da simulação");
 		frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
@@ -74,23 +82,42 @@ public class PokeMenu {
 		panelRegistradores.setLayout(null);
 		
 		txtBinarioReg = new JTextField();
-		txtBinarioReg.addFocusListener(new FocusAdapter() {
+		txtBinarioReg.getDocument().addDocumentListener(new DocumentListener() {
+
 			@Override
-			public void focusLost(FocusEvent e) {
-				spnDecimalReg.setValue(Integer.parseInt(Conversoes.bitArrayToC2(Conversoes.stringToIntegerArray(txtBinarioReg.getText()))));
+			public void insertUpdate(DocumentEvent e) {
+				javali();
+				
 			}
+
+			@Override
+			public void removeUpdate(DocumentEvent e) {
+				javali();
+				
+			}
+
+			@Override
+			public void changedUpdate(DocumentEvent e) {
+				javali();
+				
+			}
+			
+			private void javali() {
+				try {
+					spnDecimalReg.setValue(Integer.parseInt(Conversoes.bitArrayToC2(Conversoes.stringToIntegerArray(txtBinarioReg.getText()))));
+				}catch(Exception e) {
+					
+				}
+			}
+			
 		});
+		
 		txtBinarioReg.setBounds(10, 92, 281, 19);
 		panelRegistradores.add(txtBinarioReg);
 		txtBinarioReg.setColumns(16);
 		
 		spnDecimalReg = new JSpinner();
-		spnDecimalReg.addMouseListener(new MouseAdapter() {
-			@Override
-			public void mouseExited(MouseEvent e) {
-				txtBinarioReg.setText(Conversoes.conversaoCompleta((int) spnDecimalReg.getValue(), 16));
-			}
-		});
+		spnDecimalReg.setEnabled(false);
 		spnDecimalReg.setBounds(174, 47, 117, 20);
 		panelRegistradores.add(spnDecimalReg);
 		
@@ -144,5 +171,112 @@ public class PokeMenu {
 		});
 		btnAplicarReg.setBounds(206, 121, 85, 21);
 		panelRegistradores.add(btnAplicarReg);
+		
+		panelRegistradores_1 = new JPanel();
+		panelRegistradores_1.setLayout(null);
+		panelRegistradores_1.setBorder(new TitledBorder(new LineBorder(new Color(0, 0, 0)), "Mem\u00F3ria", TitledBorder.LEADING, TitledBorder.TOP, null, new Color(0, 0, 0)));
+		panelRegistradores_1.setBounds(10, 173, 301, 153);
+		frame.getContentPane().add(panelRegistradores_1);
+		
+		txtBinarioMem = new JTextField();
+		txtBinarioMem.getDocument().addDocumentListener(new DocumentListener() {
+
+			@Override
+			public void insertUpdate(DocumentEvent e) {
+				javali();
+				
+			}
+
+			@Override
+			public void removeUpdate(DocumentEvent e) {
+				javali();
+				
+			}
+
+			@Override
+			public void changedUpdate(DocumentEvent e) {
+				javali();
+				
+			}
+			
+			private void javali() {
+				try {
+					spnDecimalMem.setValue(Conversoes.binaryIntToDecimal(Conversoes.stringToIntegerArray(txtBinarioMem.getText())));
+				}catch(Exception e) {
+					
+				}
+				
+			}
+			
+		});
+		txtBinarioMem.setColumns(16);
+		txtBinarioMem.setBounds(10, 92, 281, 19);
+		panelRegistradores_1.add(txtBinarioMem);
+		
+		spnDecimalMem = new JSpinner();
+		spnDecimalMem.setEnabled(false);
+		spnDecimalMem.setBounds(174, 47, 117, 20);
+		panelRegistradores_1.add(spnDecimalMem);
+		
+		lblEndereo = new JLabel("Endereço:");
+		lblEndereo.setFont(new Font("Segoe UI", Font.PLAIN, 14));
+		lblEndereo.setBounds(10, 24, 95, 21);
+		panelRegistradores_1.add(lblEndereo);
+		
+		JLabel lblValordecimal_1 = new JLabel("Valor (decimal):");
+		lblValordecimal_1.setFont(new Font("Segoe UI", Font.PLAIN, 14));
+		lblValordecimal_1.setBounds(174, 24, 117, 21);
+		panelRegistradores_1.add(lblValordecimal_1);
+		
+		JLabel lblValorbinrio_1 = new JLabel("Valor (binário):");
+		lblValorbinrio_1.setFont(new Font("Segoe UI", Font.PLAIN, 14));
+		lblValorbinrio_1.setBounds(10, 68, 117, 21);
+		panelRegistradores_1.add(lblValorbinrio_1);
+		
+		JButton btnAplicarReg_1 = new JButton("Aplicar");
+		btnAplicarReg_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				MemoriaPrincipal mem = uc.getMemoriaPrincipal();
+				int enderecoSelecionado = Integer.parseInt(spnEndereco.getText());
+				mem.mem.set(enderecoSelecionado, Conversoes.stringToIntegerArray(txtBinarioMem.getText()));
+			}
+		});
+		btnAplicarReg_1.setBounds(206, 121, 85, 21);
+		panelRegistradores_1.add(btnAplicarReg_1);
+		
+		spnEndereco = new JTextField();
+		spnEndereco.getDocument().addDocumentListener(new DocumentListener() {
+			@Override
+			public void insertUpdate(DocumentEvent e) {
+				javali();
+				
+			}
+
+			@Override
+			public void removeUpdate(DocumentEvent e) {
+				javali();
+				
+			}
+
+			@Override
+			public void changedUpdate(DocumentEvent e) {
+				javali();
+				
+			}
+			
+			private void javali() {
+				try{
+					Integer[] memValue = uc.memoriaPrincipal.get(Integer.parseInt(spnEndereco.getText()));
+					spnDecimalMem.setValue(Conversoes.binaryIntToDecimal(memValue));
+					txtBinarioMem.setText(Conversoes.integerArrayToString(memValue));
+				}catch(Exception e) {
+					
+				}
+				
+				
+			}
+		});
+		spnEndereco.setBounds(10, 47, 154, 20);
+		panelRegistradores_1.add(spnEndereco);
 	}
 }
