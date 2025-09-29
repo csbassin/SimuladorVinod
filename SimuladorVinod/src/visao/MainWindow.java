3f9444355dbf09bd121a20f6a0eb59cbbd97ce13
Cauã Bassin <cauabassin@id.uff.br>
2025-09-29 15:59:29 -0300

    Interface quase pronta

--- SimuladorVinod/src/visao/MainWindow.java
+++ SimuladorVinod/src/visao/MainWindow.java
@@ -17,6 +17,10 @@ import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import java.awt.Font;
 import javax.swing.JScrollPane;
+import java.awt.Color;
+import javax.swing.JSeparator;
+import javax.swing.SpringLayout;
+import javax.swing.border.LineBorder;
 
 public class MainWindow {
 	
@@ -25,18 +29,12 @@ public class MainWindow {
 	private String[] title = new String[] {"Registrador", "Valor"};
 	private DefaultTableModel dtm = new DefaultTableModel(new String[0][2], title);
 	JTable tableRegisters;
-	private JLabel lblNewLabel_1_2;
-	private JTextArea txtMicro;
 	private WindowUpdater wu;
 	private UnidadeControle uc;
-	private JButton btnAplicar_1;
 	private JScrollPane scrollMP;
 	private JTable tableMP;
 	private JScrollPane scrollAssembly;
 	private JTextArea txtAssembly;
-	private JButton btnAplicar_1_1;
-	private JLabel lblTempo;
-	private JSpinner spnPausaPC;
 	
 	/**
 	 * Launch the application.
@@ -82,43 +80,32 @@ public class MainWindow {
 	 */
 	private void initialize() {
 		MainWindow currentJanela = this;
-		wu = WindowUpdater.getWu(this,new MemoriaWindow(), new ProcessadorWindow(), 1000);
+		MemoriaWindow mw = new MemoriaWindow();
+		wu = WindowUpdater.getWu(this, mw, new ProcessadorWindow(), 1000);
 		new ControlesWindow(wu);
 		uc = UnidadeControle.getUc();
 		
 		frame = new JFrame();
-		frame.setBounds(100, 100, 640, 673);
+		frame.getContentPane().setBackground(Color.DARK_GRAY);
+		frame.setBounds(100, 100, 395, 375);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.getContentPane().setLayout(null);
 		frame.setTitle("Simulador MIC-1 - Por Cauã Bassin, Lucas Avelar, Bernardo Rebello, Vitor Lemos e Allan Gaetani");
+		SpringLayout springLayout = new SpringLayout();
+		frame.getContentPane().setLayout(springLayout);
 		
 		JLabel lblNewLabel_1 = new JLabel("Compilado:");
+		springLayout.putConstraint(SpringLayout.EAST, lblNewLabel_1, 323, SpringLayout.WEST, frame.getContentPane());
+		lblNewLabel_1.setForeground(Color.WHITE);
 		lblNewLabel_1.setFont(new Font("Segoe UI", Font.PLAIN, 16));
-		lblNewLabel_1.setBounds(161, 11, 143, 26);
 		frame.getContentPane().add(lblNewLabel_1);
 		
-		JLabel lblNewLabel_1_1 = new JLabel("Registradores");
-		lblNewLabel_1_1.setFont(new Font("Segoe UI", Font.PLAIN, 16));
-		lblNewLabel_1_1.setBounds(324, 11, 137, 26);
-		frame.getContentPane().add(lblNewLabel_1_1);
-		
-		JScrollPane scrollPane = new JScrollPane();
-		scrollPane.setBounds(324, 40, 290, 184);
-		
-		frame.getContentPane().add(scrollPane);
-		
-		lblNewLabel_1_2 = new JLabel("Microinstrução");
-		lblNewLabel_1_2.setFont(new Font("Segoe UI", Font.PLAIN, 16));
-		lblNewLabel_1_2.setBounds(324, 235, 137, 26);
-		frame.getContentPane().add(lblNewLabel_1_2);
-		
-		txtMicro = new JTextArea();
-		txtMicro.setWrapStyleWord(true);
-		txtMicro.setLineWrap(true);
-		txtMicro.setBounds(324, 260, 290, 53);
-		frame.getContentPane().add(txtMicro);
-		
 		JButton btnGravarCdigoNa = new JButton("Gravar código na MP");
+		btnGravarCdigoNa.setBorder(new LineBorder(Color.WHITE, 2));
+		btnGravarCdigoNa.setContentAreaFilled(false);
+		btnGravarCdigoNa.setForeground(Color.WHITE);
+		btnGravarCdigoNa.setFont(new Font("Segoe UI", Font.PLAIN, 14));
+		springLayout.putConstraint(SpringLayout.WEST, btnGravarCdigoNa, 20, SpringLayout.WEST, frame.getContentPane());
+		springLayout.putConstraint(SpringLayout.EAST, btnGravarCdigoNa, -20, SpringLayout.EAST, frame.getContentPane());
 		btnGravarCdigoNa.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				String[] programa = txtCode.getText().split("\n");
@@ -126,25 +113,18 @@ public class MainWindow {
 					uc.memoriaPrincipal.setEnderecoSlecionado(i);
 					uc.memoriaPrincipal.write(Conversoes.stringToIntegerArray(programa[i]));
 				}
-
+				mw.update();
 			}
 		});
-		btnGravarCdigoNa.setBounds(10, 290, 212, 23);
 		frame.getContentPane().add(btnGravarCdigoNa);
-		
-		btnAplicar_1 = new JButton("Iniciar");
-		btnAplicar_1.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				uc.start();
-				wu.start();
-			}
-		});
-		btnAplicar_1.setBounds(231, 290, 72, 23);
-		frame.getContentPane().add(btnAplicar_1);
 	
 		
 		JScrollPane scrollPane_1 = new JScrollPane();
-		scrollPane_1.setBounds(161, 40, 153, 185);
+		springLayout.putConstraint(SpringLayout.WEST, lblNewLabel_1, 0, SpringLayout.WEST, scrollPane_1);
+		springLayout.putConstraint(SpringLayout.WEST, scrollPane_1, 180, SpringLayout.WEST, frame.getContentPane());
+		springLayout.putConstraint(SpringLayout.NORTH, scrollPane_1, 78, SpringLayout.NORTH, frame.getContentPane());
+		springLayout.putConstraint(SpringLayout.SOUTH, scrollPane_1, 263, SpringLayout.NORTH, frame.getContentPane());
+		springLayout.putConstraint(SpringLayout.EAST, scrollPane_1, -20, SpringLayout.EAST, frame.getContentPane());
 		frame.getContentPane().add(scrollPane_1);
 		
 		txtCode = new JTextArea();
@@ -153,7 +133,10 @@ public class MainWindow {
 		txtCode.setLineWrap(true);
 		
 		scrollAssembly = new JScrollPane();
-		scrollAssembly.setBounds(10, 40, 143, 185);
+		springLayout.putConstraint(SpringLayout.NORTH, scrollAssembly, 78, SpringLayout.NORTH, frame.getContentPane());
+		springLayout.putConstraint(SpringLayout.WEST, scrollAssembly, 20, SpringLayout.WEST, frame.getContentPane());
+		springLayout.putConstraint(SpringLayout.SOUTH, scrollAssembly, 263, SpringLayout.NORTH, frame.getContentPane());
+		springLayout.putConstraint(SpringLayout.EAST, scrollAssembly, 170, SpringLayout.WEST, frame.getContentPane());
 		frame.getContentPane().add(scrollAssembly);
 		
 		txtAssembly = new JTextArea();
@@ -162,69 +145,49 @@ public class MainWindow {
 		scrollAssembly.setViewportView(txtAssembly);
 		
 		JLabel lblNewLabel_1_3 = new JLabel("Assembly:");
+		springLayout.putConstraint(SpringLayout.NORTH, lblNewLabel_1, 0, SpringLayout.NORTH, lblNewLabel_1_3);
+		springLayout.putConstraint(SpringLayout.NORTH, lblNewLabel_1_3, 52, SpringLayout.NORTH, frame.getContentPane());
+		springLayout.putConstraint(SpringLayout.WEST, lblNewLabel_1_3, 20, SpringLayout.WEST, frame.getContentPane());
+		springLayout.putConstraint(SpringLayout.SOUTH, lblNewLabel_1_3, 78, SpringLayout.NORTH, frame.getContentPane());
+		springLayout.putConstraint(SpringLayout.EAST, lblNewLabel_1_3, 163, SpringLayout.WEST, frame.getContentPane());
+		lblNewLabel_1_3.setForeground(Color.WHITE);
 		lblNewLabel_1_3.setFont(new Font("Segoe UI", Font.PLAIN, 16));
-		lblNewLabel_1_3.setBounds(10, 11, 153, 26);
 		frame.getContentPane().add(lblNewLabel_1_3);
 		
 		JButton btnMontar = new JButton("Montar");
+		btnMontar.setForeground(Color.WHITE);
+		btnMontar.setFont(new Font("Segoe UI", Font.PLAIN, 14));
+		btnMontar.setContentAreaFilled(false);
+		btnMontar.setBorder(new LineBorder(Color.WHITE, 2));
+		springLayout.putConstraint(SpringLayout.WEST, btnMontar, 20, SpringLayout.WEST, frame.getContentPane());
+		springLayout.putConstraint(SpringLayout.NORTH, btnGravarCdigoNa, 6, SpringLayout.SOUTH, btnMontar);
+		springLayout.putConstraint(SpringLayout.NORTH, btnMontar, 6, SpringLayout.SOUTH, scrollPane_1);
+		springLayout.putConstraint(SpringLayout.EAST, btnMontar, -20, SpringLayout.EAST, frame.getContentPane());
 		btnMontar.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				Montador montador = new Montador(txtAssembly.getText(), currentJanela);
 				montador.start();
 			}
 		});
-		btnMontar.setBounds(10, 259, 72, 23);
 		frame.getContentPane().add(btnMontar);
 		
-		btnAplicar_1_1 = new JButton("Editar Valores");
-		btnAplicar_1_1.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				new PokeMenu(uc);
-			}
-		});
-		btnAplicar_1_1.setBounds(485, 570, 129, 23);
-		frame.getContentPane().add(btnAplicar_1_1);
+		JLabel lblPrograma = new JLabel("Programa");
+		springLayout.putConstraint(SpringLayout.WEST, lblPrograma, 10, SpringLayout.WEST, frame.getContentPane());
+		springLayout.putConstraint(SpringLayout.SOUTH, lblPrograma, -6, SpringLayout.NORTH, lblNewLabel_1);
+		springLayout.putConstraint(SpringLayout.EAST, lblPrograma, 300, SpringLayout.WEST, frame.getContentPane());
+		lblPrograma.setForeground(Color.WHITE);
+		lblPrograma.setFont(new Font("Segoe UI", Font.PLAIN, 28));
+		frame.getContentPane().add(lblPrograma);
 		
-		lblTempo = new JLabel("Tempo de Execução: 0ms");
-		lblTempo.setFont(new Font("Segoe UI", Font.PLAIN, 16));
-		lblTempo.setBounds(10, 569, 372, 24);
-		frame.getContentPane().add(lblTempo);
-		
-		JLabel lblNewLabel_1_2_1 = new JLabel("Pausar automaticamente quando PC:");
-		lblNewLabel_1_2_1.setFont(new Font("Segoe UI", Font.PLAIN, 16));
-		lblNewLabel_1_2_1.setBounds(10, 598, 264, 26);
-		frame.getContentPane().add(lblNewLabel_1_2_1);
-		
-		spnPausaPC = new JSpinner();
-		spnPausaPC.setBounds(284, 603, 177, 20);
-		frame.getContentPane().add(spnPausaPC);
-		
-		JButton btnAplicar_1_1_1 = new JButton("Aplicar");
-		btnAplicar_1_1_1.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				uc.setPcValueForPause(Integer.parseInt(spnPausaPC.getValue().toString()));
-			}
-		});
-		btnAplicar_1_1_1.setBounds(485, 603, 129, 23);
-		frame.getContentPane().add(btnAplicar_1_1_1);
+		JSeparator separator = new JSeparator();
+		springLayout.putConstraint(SpringLayout.NORTH, separator, 47, SpringLayout.NORTH, frame.getContentPane());
+		springLayout.putConstraint(SpringLayout.WEST, separator, 10, SpringLayout.WEST, frame.getContentPane());
+		springLayout.putConstraint(SpringLayout.EAST, separator, -10, SpringLayout.EAST, frame.getContentPane());
+		frame.getContentPane().add(separator);
 	}
 	
 	public void update() {
-		/*dtm.setValueAt(WindowData.pc, 0, 1);
-		dtm.setValueAt(WindowData.ac, 1, 1);
-		dtm.setValueAt(WindowData.sp, 2, 1);
-		dtm.setValueAt(WindowData.ir, 3, 1);
-		dtm.setValueAt(WindowData.tir, 4, 1);
-		dtm.setValueAt(WindowData.a, 5, 1);
-		dtm.setValueAt(WindowData.b, 6, 1);
-		dtm.setValueAt(WindowData.c, 7, 1);
-		dtm.setValueAt(WindowData.d, 8, 1);
-		dtm.setValueAt(WindowData.e, 9, 1);
-		dtm.setValueAt(WindowData.f, 10, 1);*/
-		txtMicro.setText("Subciclo atual: "+WindowData.currentSub+"\nMicroinstrução atual: "+WindowData.microAtual);
-		lblTempo.setText("Tempo de execução: "+WindowData.executionTime+"ms");
-		
-		
+
 	}
 	public void setTxtCode(String code) {
 		txtCode.setText(code);
